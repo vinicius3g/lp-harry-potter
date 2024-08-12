a8a25ce327748548af1bacba03c140326c55fb71
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2024-08-12 15:38:09 -0300

    feat(App): aquivo responsavel por fazer a requisiçao e renderizar so cards de acordo com as informaçoes retorndas

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,64 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import styles from './App.module.scss';
+import CharacterCard from "./CharacterCard/CharacterCard";
+
+interface Character {
+  id: string;
+  name: string;
+  dateOfBirth: string;
+  house: string;
+  patronus: string;
+  actor: string;
+  alive: boolean;
+  image: string;
+}
 
 function App() {
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [visibleCharacters, setVisibleCharacters] = useState<Character[]>([]);
+  const [loadMore, setLoadMore] = useState(20);
+
+  useEffect(() => {
+    fetch('https://hp-api.onrender.com/api/characters')
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacters(data);
+        setVisibleCharacters(data.slice(0, loadMore));
+      })
+      .catch((error) => console.error('Erro ao buscar dados:', error));
+  }, []);
+
+  const handleLoadMore = () => {
+    const newLoadMore = loadMore + 20;
+    setLoadMore(newLoadMore);
+    setVisibleCharacters(characters.slice(0, newLoadMore));
+  };
+
   return (
-    <div className="App">
-      <div>test</div>
-    </div>
+    <>
+      <div className={styles.container}>
+        {visibleCharacters.map((character) => (
+          <CharacterCard
+            key={character.id}
+            name={character.name}
+            image={character.image}
+            dateOfBirth={character.dateOfBirth}
+            house={character.house}
+            patronus={character.patronus}
+            actor={character.actor}
+            alive={character.alive}
+          />
+        ))}
+      </div>
+
+      {loadMore < characters.length && (
+        <div className={styles.buttonWrapper}>
+          <button className={styles.loadMoreButton} onClick={handleLoadMore}>
+            <span>Carregar Mais</span>
+          </button>
+        </div>
+      )}
+    </>
   );
 }
 
